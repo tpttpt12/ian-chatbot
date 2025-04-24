47c1a3624683f7bc420aedca609457d79baeb679
tpttpt12 <103556363+tpttpt12@users.noreply.github.com>
2025-04-24 21:00:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -893,7 +893,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     // 메뉴 오버레이 클릭 시 메뉴 닫기
-    console.log('Checking menuOverlay:', menuOverlay);s
+    console.log('Checking menuOverlay:', menuOverlay); // 올바른 코드
     menuOverlay.addEventListener("click", function() {
         actionMenu.classList.remove("visible");
         menuOverlay.style.display = 'none';
